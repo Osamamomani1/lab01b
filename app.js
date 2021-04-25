0dc9448ec0216c128cd3218f78cd59d14172beab
Osamamomani1 <momaniosama97@gmail.com>
2021-04-25 14:19:07 +0300

    labwork

---
+++ app.js
@@ -0,0 +1,15 @@
+let userName = prompt ('what\'s your name ?') ;
+console.log('your name is '+userName);
+alert ('your name is '+userName);
+
+let faVclub = prompt ('what\'s your favorite football club ?');
+console.log('your favorite football club is '+faVclub);
+alert ('your name is '+userName+', you are a fan of '+faVclub);
+
+let faVplayer = prompt ('who is your favorite player ?');
+console.log('your favorite player is '+faVplayer);
+alert ('your name is '+userName+', you are a fan of '+faVclub+' your favorite player is '+faVplayer);
+
+let bestPlayer = prompt ('who you think is the best player in the world ?');
+console.log('the best player in the world is '+bestPlayer)
+alert ('your name is '+userName+', you are a fan of '+faVclub+' your favorite player is '+faVplayer+' and you think '+bestPlayer+' is the best player in the world');
